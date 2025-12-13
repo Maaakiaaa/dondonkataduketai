2b58a050dc9e157c02cb8182edd9aca0f4bf4d56
とばくろ <tobakurow57@gmail.com>
2025-12-13 15:37:16 +0900

    Spotifyのログイン認証で使用

---
+++ app/music/api/login/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import spotifyApi from "../../lib/spotify";
+
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "user-library-read",
+  "user-top-read",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+  "playlist-modify-public",
+  "playlist-modify-private",
+];
+
+export async function GET() {
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes, "state");
+  return NextResponse.json({ url: authorizeURL });
+}
