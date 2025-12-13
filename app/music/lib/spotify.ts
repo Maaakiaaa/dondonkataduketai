209656df763c627abb1fe68fa8d156d7bdae8474
とばくろ <tobakurow57@gmail.com>
2025-12-13 15:36:19 +0900

    SpotifyAPIのクライアント設定, 環境変数の読込とか.

---
+++ app/music/lib/spotify.ts
@@ -0,0 +1,9 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: `${process.env.NEXTAUTH_URL}/music/api/callback`,
+});
+
+export default spotifyApi;
